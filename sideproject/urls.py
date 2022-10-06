c26bab01203a475986a0c83f2273987979e5cf98
hyeonjin <choihj94@gmail.com>
2022-10-07 00:33:07 +0900

    [MOD] root urls

--- sideproject/urls.py
+++ sideproject/urls.py
@@ -1,7 +1,21 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from django.urls import include, path
+from django.views.generic import TemplateView
+from django_pydenticon.views import image as pydenticon_image
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('identicon/image/<path:data>/', pydenticon_image, name='pydenticon_image'),
+    path('', login_required(TemplateView.as_view(template_name='root.html')), name='root'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        # ...
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
