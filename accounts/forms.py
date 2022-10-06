5ef53622ed2e8aed42ecda12949c675414842982
hyeonjin <choihj94@gmail.com>
2022-10-07 01:03:22 +0900

    [ADD] password change

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import password_validation
-from django.contrib.auth.forms import UsernameField
+from django.contrib.auth.forms import (UsernameField, PasswordChangeForm as AuthPasswordChangeForm)
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -87,3 +87,14 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['avatar', 'first_name', 'last_name', 'website_url', 'bio', 'phone_number', 'gender']
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password1 = self.cleaned_data.get('new_password1')
+        if old_password == new_password1:
+            raise forms.ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+        return new_password1
+
